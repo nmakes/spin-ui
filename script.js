821e379c3628b681b7caa8e53998abf92276836d
nmakes <nav.naveenvenkat@gmail.com>
2017-03-03 18:25:39 +0000

    added js at the end, added tabEvent in js file

--- script.js
+++ script.js
@@ -15,6 +15,32 @@ function resize(){
 	}
 }
 
+var textArea = document.getElementById('spin-text-area');
+
+textArea.addEventListener('keydown',tabEvent); 
+// by default addEventListener passes the event as an argument to the function tabEvent
+
+/*
+// default parameter passing test
+
+var hi=[6,24,5];
+hi.map(function (i,ele) {
+	console.log(ele,i);
+});
+*/
+
+function tabEvent(event) {
+	if(event.keyCode===9){
+		event.preventDefault();
+		var v=this.value,
+			s=this.selectionStart,
+			e=this.selectionEnd;
+		this.value=v.substring(0, s)+'    '+v.substring(e);
+		this.selectionStart=this.selectionEnd=s+4;
+		return false;
+	}
+}
+
 function clearSpinTextArea(){
 	document.getElementById('spin-text-area').value = '';
 }
